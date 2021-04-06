a81183698d37954146d02294625a6cc7f1d8fb0a
Clemente <cross3@uc.cl>
2021-04-06 17:59:22 -0400

    heroku again

--- tarea1/settings.py
+++ tarea1/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'tarea1-cross3.herokuapp.com']
 
 
 # Application definition
