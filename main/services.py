b1c7c3c2939c7f81f179ca794f01eb155e4c680b
Clemente <cross3@uc.cl>
2021-04-07 16:36:43 -0400

    manejo 429

--- main/services.py
+++ main/services.py
@@ -9,10 +9,15 @@ def generate_request(url, params={}):
 
     if response.status_code == 200:
         return response.json()
+    
+    elif response.status_code == 429:
+        return '429 ERROR'
 
 
 def get_seasons(params={}):
     episodes = generate_request("%sepisodes" % API_URL, params)
+    if episodes == '429 ERROR':
+        return '429 ERROR'
     last_season = int(episodes[-1]["season"])
     seasons_list = [i for i in range(1, last_season + 1)]
     return seasons_list
@@ -20,6 +25,8 @@ def get_seasons(params={}):
 
 def get_episodes(params={}):
     episodes = generate_request("%sepisodes" % API_URL, {'series': params['series']})
+    if episodes == '429 ERROR':
+        return '429 ERROR'
     filter_episodes = list(filter(lambda elem: int(elem["season"]) == params["season"], episodes))
     episodes_titles = [elem["title"] for elem in filter_episodes]
     episodes_id = [elem["episode_id"] for elem in filter_episodes]
@@ -28,16 +35,22 @@ def get_episodes(params={}):
 
 def get_episode_by_id(episode_id, params={}):
     episode_detail = generate_request("%sepisodes/%s" % (API_URL, episode_id), params)
+    if episode_detail == '429 ERROR':
+        return '429 ERROR'
     return episode_detail
 
 
 def get_character_by_name(params={}):
     character_detail = generate_request("%scharacters" % API_URL, params)
+    if character_detail == '429 ERROR':
+        return '429 ERROR'
     return character_detail
 
 
 def get_quotes_by_author(params={}):
     quotes = generate_request("%squote" % API_URL, params)
+    if quotes == '429 ERROR':
+        return '429 ERROR'
     return quotes
 
 
@@ -47,6 +60,8 @@ def get_characters_by_search(params={}):
     characters_list = []
     while True:
         sample_list = generate_request("%scharacters" % API_URL, params)
+        if sample_list == '429 ERROR':
+            return '429 ERROR'
         characters_list += sample_list
         if len(sample_list) < 10:
             break
--- main/views.py
+++ main/views.py
@@ -6,38 +6,51 @@ import datetime
 def seasons(request):
     seasons_bb_list = get_seasons(params={"series": "Breaking Bad"})
     seasons_bcs_list = get_seasons(params={"series": "Better Call Saul"})
-  
-    context = {
-        'seasons_bb_list': seasons_bb_list,
-        'seasons_bcs_list': seasons_bcs_list,
-    }
+    if seasons_bb_list == '429 ERROR' or seasons_bcs_list == '429 ERROR':
+        context = {
+            'error_message': 'Lo siento, la API ha superado el límite de consultas, pruebe de nuevo más tarde.'
+        }
+    else:
+        context = {
+            'seasons_bb_list': seasons_bb_list,
+            'seasons_bcs_list': seasons_bcs_list,
+        }
     return render(request, 'main/seasons.html', context)
 
 
 def episodes(request, series, season):
     episodes_zip = get_episodes(params={"series": series, "season": season})
-
-    context = {
-        'series': series,
-        'season': season,
-        'episodes_zip': episodes_zip,
-    }
+    if episodes_zip == '429 ERROR':
+        context = {
+            'error_message': 'Lo siento, la API ha superado el límite de consultas, pruebe de nuevo más tarde.'
+        }
+    else:
+        context = {
+            'series': series,
+            'season': season,
+            'episodes_zip': episodes_zip,
+        }
     return render(request, 'main/episodes.html', context)
 
 
 def episode_detail(request, episode_id):
     episode_detail = get_episode_by_id(episode_id)
     if episode_detail:
-        episode_detail = episode_detail[0]
-        context = {
-            'episode_id': episode_detail['episode_id'],
-            'title': episode_detail['title'],
-            'season': episode_detail['season'],
-            'air_date': datetime.datetime.strptime(episode_detail['air_date'],"%Y-%m-%dT%H:%M:%S.%fZ"),
-            'characters': episode_detail['characters'],
-            'episode': episode_detail['episode'],
-            'series': episode_detail['series'],
+        if episode_detail == '429 ERROR':
+            context = {
+                'error_message': 'Lo siento, la API ha superado el límite de consultas, pruebe de nuevo más tarde.'
         }
+        else:
+            episode_detail = episode_detail[0]
+            context = {
+                'episode_id': episode_detail['episode_id'],
+                'title': episode_detail['title'],
+                'season': episode_detail['season'],
+                'air_date': datetime.datetime.strptime(episode_detail['air_date'],"%Y-%m-%dT%H:%M:%S.%fZ"),
+                'characters': episode_detail['characters'],
+                'episode': episode_detail['episode'],
+                'series': episode_detail['series'],
+            }
     else:
         context = {
             'episode_id': None,
@@ -48,24 +61,29 @@ def episode_detail(request, episode_id):
 def character_detail(request, name):
     character_detail = get_character_by_name(params={'name': name})
     if character_detail:
-        character_detail = character_detail[0]
-
-        quotes_info = get_quotes_by_author(params={'author': name})
-        quotes = [elem['quote'] for elem in quotes_info]
-
-        context = {
-            'char_id': character_detail['char_id'],
-            'name': character_detail['name'],
-            'occupation': character_detail['occupation'],
-            'img': character_detail['img'],
-            'status': character_detail['status'],
-            'nickname': character_detail['nickname'],
-            'appearance': character_detail['appearance'],
-            'portrayed': character_detail['portrayed'],
-            'category': character_detail['category'],
-            'better_call_saul_appearance': character_detail['better_call_saul_appearance'],
-            'quotes': quotes,
+        if character_detail == '429 ERROR':
+            context = {
+                'error_message': 'Lo siento, la API ha superado el límite de consultas, pruebe de nuevo más tarde.'
         }
+        else:
+            character_detail = character_detail[0]
+
+            quotes_info = get_quotes_by_author(params={'author': name})
+            quotes = [elem['quote'] for elem in quotes_info]
+
+            context = {
+                'char_id': character_detail['char_id'],
+                'name': character_detail['name'],
+                'occupation': character_detail['occupation'],
+                'img': character_detail['img'],
+                'status': character_detail['status'],
+                'nickname': character_detail['nickname'],
+                'appearance': character_detail['appearance'],
+                'portrayed': character_detail['portrayed'],
+                'category': character_detail['category'],
+                'better_call_saul_appearance': character_detail['better_call_saul_appearance'],
+                'quotes': quotes,
+            }
     else:
         context = {
             'char_id': None,
@@ -76,9 +94,14 @@ def character_list(request):
     name = request.POST.get('name', '')
     if name:
         characters_list = get_characters_by_search(params={'name': name})
-        context = {
-            'characters_list': characters_list,
+        if characters_list == '429 ERROR':
+            context = {
+                'error_message': 'Lo siento, la API ha superado el límite de consultas, pruebe de nuevo más tarde.'
         }
+        else:
+            context = {
+                'characters_list': characters_list,
+            }
     else:
         context = {
             'characters_list': None,
